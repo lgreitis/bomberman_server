108937d9289d77f864c02275c0bec5bcd4b97365
Marius Povilonis <povilonismarius@gmail.com>
2022-11-05 22:14:45 +0200

    adapter

---
+++ Game/Models/MapModels/Adapter/PropAdapter.cs
@@ -0,0 +1,23 @@
+﻿using GameServices.Interfaces;
+using GameServices.Models.CommonModels;
+
+namespace GameServices.Models.MapModels.Adapter
+{
+    public class PropAdapter : IBomb
+    {
+        public IProp Prop { get; set; }
+        public Position PlacedPosition { get; set; }
+        public bool IsPlaced { get; set; }
+
+        public List<Position> Activate()
+        {
+            return Prop.GetAffectedPositions(PlacedPosition);
+        }
+
+        public void Place(Position position)
+        {
+            this.PlacedPosition = position;
+            IsPlaced = true;
+        }
+    }
+}
