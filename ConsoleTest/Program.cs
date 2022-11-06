7f81e67f1edc416d6c0f07df0d5efe1a6aaac253
mduoblys <mduoblys@gmail.com>
2022-11-07 01:46:02 +0200

    Prototype memory address in ConsoleTest

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using GameServices.Models.BombModels;
+using GameServices.Models.CommonModels;
+
+var bomb = new RegularBomb();
+bomb.PlacedPosition = new Position(1, 1);
+var bombpos = bomb.PlacedPosition;
+
+var deep = (RegularBomb)bomb.DeepCopy();
+var deeppos = deep.PlacedPosition;
+
+var shallow = (RegularBomb)bomb.ShallowCopy();
+var shallowpos = shallow.PlacedPosition;
+
+Console.ReadKey();
